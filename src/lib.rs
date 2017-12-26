f3d8d82ed1ce2efb58f0919544379d50706fb433
heySasha <iatsenko.o@donnu.edu.ua>
2017-12-26 09:30:58 +0200

    added vec_to_file, file_to_vec

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
+fn file_to_vec(file_path: &str, v: &mut Vec<u8>) {
+    let mut file= match File::open(file_path) {
+        Result::Ok(val) => val,
+        Result::Err(err) => panic!("{:?}", err)
+    };
+
+    match file.read_to_end(v) {
+        Ok(val) => val,
+        Err(err) => panic!("{:?}", err)
+    };
+}
+
+
+fn vec_to_file(file_path: &str, v: &mut Vec<u8>) {
+    let mut file_out= match File::create(file_path) {
+        Result::Ok(val) => val,
+        Result::Err(err) => panic!("{:?}", err)
+    };
+
+    match file_out.write_all(v) {
+        Result::Ok(val) => val,
+        Result::Err(err) => panic!("{:?}", err)
+    };
+}
